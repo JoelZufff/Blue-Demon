abe1849259212dcdd9f03f3e39c66cb2a229800e
Joel_Zufff <diegodjzf@gmail.com>
2024-03-09 18:31:09 -0600

    Subo datos del proyecto

---
+++ Programacion/main.c
@@ -0,0 +1,25 @@
+// --------------- Preprocesadores de microcontrolador -------------- //
+#include    <18f4550.h>                                                 // Libreria del Microcontrolador
+#fuses      INTRC, CPUDIV1, PLL1, NOWDT, NOPROTECT, NOLVP, NOMCLR       // Fusibles (Configuraciones del microcontrolador)
+#use        delay(clock = 8M)                                           // Configuracion de frecuencia y delay
+
+// --------------------- Direccion de registros --------------------- //
+
+
+// ----------------------- Variable Globales ------------------------ //
+
+
+// -------------------------- Interrupciones ------------------------ //
+
+
+// ---------------------------- Funciones --------------------------- //
+
+
+// ------------------------ Codigo Principal ----------------------- //
+void main()
+{
+    while (TRUE)
+    {
+        /* code */
+    }
+}
