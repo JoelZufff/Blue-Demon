480efd920da23c0e7ce67f417a819262e581637d
Joel_Zufff <diegodjzf@gmail.com>
2024-04-07 18:57:58 -0600

    Firmware: Mejore organizacion y cree funciones para movimiento de motor con el modulo HW-166

--- Firmware/Code/main.c
+++ Firmware/Code/main.c
@@ -4,6 +4,8 @@
 #use        delay(internal = 16MHz)                                     // Configuracion de frecuencia y delay
 //#use        rs232(rcv = pin_b7, xmit = pin_b6, baud = 9600, bits = 8, parity = n) 
 
+#include    "motor_control.c"
+
 #define RIGHT   1
 #define LEFT    0
 
@@ -18,31 +20,16 @@ const int16 *ADRES          = 0xFC3;
 #BIT        DONE            = 0xFC2.1
 #BYTE       ADCON2          = 0xFC0
 
-#BYTE       CCPTMRS0        = 0xF49
-#BYTE       CCPTMRS1        = 0xF48
-
-#BYTE       CCP1CON         = 0xFBD
-#BYTE       CCP2CON         = 0xF66
-#BYTE       CCP3CON         = 0xF5D
-#BYTE       CCP4CON         = 0xF57
-
-#BYTE       T2CON           = 0xFBA
-#BYTE       PR2             = 0xFBB
-
 // -------------------------- Estructuras --------------------------- //
-struct wheel_t
-{
-    int     *DutyCicle;
-    long    in_pins[2];
-};
+
 
 // ----------------------- Variable Globales ------------------------ //
-struct wheel_t wheels[4] = 
+struct motor_t wheels[4] = 
 {
-    { 0xFBE, { PIN_B1, PIN_B2 } },  /* Rueda A */ 
-    { 0xF67, { PIN_B3, PIN_B4 } },  /* Rueda B */
-    { 0xF5E, { PIN_B5, PIN_B6 } },  /* Rueda C */
-    { 0xF58, { PIN_C3, PIN_C4 } }   /* Rueda D */
+    { 0xFBE, PIN_B1, PIN_B2 },  /* Rueda A */ 
+    { 0xF67, PIN_B3, PIN_B4 },  /* Rueda B */
+    { 0xF5E, PIN_B5, PIN_B6 },  /* Rueda C */
+    { 0xF58, PIN_C3, PIN_C4 }   /* Rueda D */
 };
 
 // -------------------------- Interrupciones ------------------------ //
@@ -59,51 +46,29 @@ void log_init()
     // Configuracion de ADC
     ADCON0      = 0b00000011;
     ADCON2      = 0b10000000;
-
-    // Configuracion de Timers
-    T2CON       = 0b00000111;
-    PR2         = 249;
-    
-    // Configuracion de CCPXCON para PWM
-    CCPTMRS0 = CCPTMRS0 = 0b00000000;       // Configuramos CCPX con timer 2
-    CCP1CON = CCP2CON = CCP3CON = CCP4CON = 0b00001100;
-}
-
-void wheel_movement(int wheel_index, int1 direction)
-{
-    output_bit(wheels[wheel_index].in_pins[0], direction);
-    output_bit(wheels[wheel_index].in_pins[1], !direction);
 }
 
 // ------------------------ Codigo Principal ----------------------- //
 void main()
 {
     log_init();
+    motors_init(4);
 
     int1 direction = LEFT;
-    int16 ADC;
-
-    for(int i = 0; i < 4 ; i++)
-        wheel_movement(i, direction);
 
     while (TRUE)
     {
-        ADCON0  = 0b00000011;
-        delay_us(10);
-        ADC = (*ADRES / 1023.0 * 1000);
+        // Solicitamos y almacenamos lectura de ADC
+        DONE = TRUE, delay_us(10);
+        //velocity = (*ADRES / 1023.0);
         
         for(int i = 0; i < 4 ; i++)
-            (wheels[i])->DutyCicle = ADC >> 2;
+            motor_movement( wheels[i], (i < 2 ? direction : !direction), *ADRES / 1023.0 );
 
         if(BUTTON)
         {
             while(BUTTON);
             direction ^= 1;
-            
-            wheel_movement(0, direction);
-            wheel_movement(1, direction);
-            wheel_movement(2, direction);
-            wheel_movement(3, direction);
         }
     }
 }
